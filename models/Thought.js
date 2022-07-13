6cc9ab6ef01dedbf45d44160559b6e14241af033
Andrea Fang <arhughes43@gmail.com>
2022-07-13 16:16:31 -0700

    initial export User and Thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,20 @@
 const { Schema, model } = require('mongoose');
+const formatDate = require('../utils/formatDate');
 
 const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        validate: [({ length }) => length >= 1 && length <= 280, 'Outside thought text limit.']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => formatDate(createdAtVal)
 
+    }
 });
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
